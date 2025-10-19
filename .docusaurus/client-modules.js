639324b25dcca8ac45b95f2ee608cc22ef33b469
jxr <2308030528@qq.com>
2025-10-19 12:40:13 +0800

    Deploy website - based on a607d4651e24071a23435bff42b871bf60d4366d

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("E:\\code\\ospp\\docusaurus\\my-website\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("E:\\code\\ospp\\docusaurus\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("E:\\code\\ospp\\docusaurus\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("E:\\code\\ospp\\docusaurus\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("E:\\code\\ospp\\docusaurus\\my-website\\src\\css\\custom.css"),
-];
